76d2d6b101ac9d478d1dc1a5da362b6817acf483
郭舒欣 <guoshuxin@corp.netease.com>
2020-09-09 11:53:04 +0800

    test publish-2

--- packages/uouin-module-2/index.js
+++ packages/uouin-module-2/index.js
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2020-09-09 11:20:53
- * @LastEditTime: 2020-09-09 11:22:58
- * @LastEditors: your name
+ * @LastEditTime: 2020-09-09 11:49:58
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /lerna_project/packages/module-2/index.js
  */
+// const API = 'https://yande.re/post/popular_recent.json';
 const API = 'https://yande.re/post/popular_recent.json';
 
 module.exports = {
